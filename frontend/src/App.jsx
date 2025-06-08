28e77dda65e6d7dc34ac39a5528548b58a31fd04
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-06-08 13:04:18 +0100

    initializing frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="container">
+            <h1>Build Your First Dapp</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
